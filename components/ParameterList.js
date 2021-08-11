7a5520cfcfb4010901a2177d7f2d6b411f6554f3
Robby <buckdestructor@gmail.com>
2021-08-11 00:10:08 -0400

    More parameter list responsiveness

--- components/ParameterList.js
+++ components/ParameterList.js
@@ -14,7 +14,7 @@ const ParameterList = ({onRandomList, onNewList, onClear, onDupesCheck, onInstru
         <div className={styles.container} id="paramsDiv">
             <Container fluid >
                 <Row>
-                    <Col align="center" md={{ span: 6, offset: 3}}>
+                    <Col align="center" xl={{ span: 6, offset: 3}} style={{marginBottom: '1rem'}}>
                         <Form className={styles.formOverride}>
                             <Form.Label>Start with a Template</Form.Label>
                             <Button style={{marginTop: '.5rem'}} onClick={() => {onTemplateModal()}}>Templates</Button>
@@ -22,12 +22,12 @@ const ParameterList = ({onRandomList, onNewList, onClear, onDupesCheck, onInstru
                     </Col>
                 </Row>
                 <Row>
-                    <Col align="center" style={{marginTop:'.5rem'}}>
+                    <Col align="center" style={{marginTop:'.5rem', marginBottom: '1rem'}}>
                         <Form.Label style={{fontSize: '1.5rem'}}>Or</Form.Label>
                     </Col>
                 </Row>
                 <Row>
-                    <Col align="center">
+                    <Col align="center" style={{marginBottom: '1rem', minWidth: '20rem'}}>
                         <Form className={styles.formOverride}>
                             <Form.Label>Generate a new list of instruments</Form.Label>
                             <Form.Control onKeyPress={(event) => {if (!/[0-9]/.test(event.key)) {event.preventDefault();}}} className={styles.formControlOverride} type="number" placeholder="Minimum" min="0" value={minNumberState} onChange={(e) => setMinNumberState(e.target.value)}/>
@@ -35,22 +35,21 @@ const ParameterList = ({onRandomList, onNewList, onClear, onDupesCheck, onInstru
                             <Button style={{marginTop: '.5rem'}} type="button" onClick={() => {onRandomList(minNumberState, maxNumberState); setMaxNumberState(''); setMinNumberState('')}}>Generate Random List</Button>
                         </Form>
                     </Col>
-                    <Col align="center">
+                    <Col align="center" style={{marginBottom: '1rem' , minWidth: '20rem'}}>
                         <Form className={styles.formOverride}>
                             <Form.Label>Add a random group of new instruments</Form.Label>
                             <Form.Control onKeyPress={(event) => {if (!/[0-9]/.test(event.key)) {event.preventDefault();}}} className={styles.formControlOverride} type="number" placeholder="Amount of Instruments" min="0" value={newListState} onChange={(e) => setNewListState(e.target.value)}/>
                             <Button type="button" onClick={() => {onNewList(newListState); setNewListState('')}}>Generate Random Instrument(s)</Button>
                         </Form>
                     </Col>
-
-                    <Col align="center">
+                    <Col align="center" style={{marginBottom: '1rem', minWidth: '20rem'}}>
                         <Form className={styles.formOverride}>
                             <Form.Label>Choose an instrument to add to the list</Form.Label>
                             <Button onClick={() => {onInstrumentModal()}}>Select Instrument</Button>
                         </Form>
                     </Col>
 
-                    <Col align="center">
+                    <Col align="center" style={{marginBottom: '1rem', minWidth: '20rem'}}>
                         <Form className={styles.formOverride}>
                             <Form.Label>Other Parameters</Form.Label>
                             <Form.Check onChange={onDupesCheck} className={styles.formCheckOverride} label="No Duplicates?" id="noDupes"/>
