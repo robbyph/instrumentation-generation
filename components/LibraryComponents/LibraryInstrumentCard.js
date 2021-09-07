73db4116326324c08b26208b803611ba3ecc79e9
Robby <buckdestructor@gmail.com>
2021-09-07 11:11:26 -0400

    library card height fix

--- components/LibraryComponents/LibraryInstrumentCard.js
+++ components/LibraryComponents/LibraryInstrumentCard.js
@@ -5,7 +5,7 @@ const LibraryInstrumentCard = ({id, name, description, imagePath, wikiLink, tube
     
 
     return (
-            <Card style={{minWidth: '16rem', flexGrow: 1, margin:'1rem', minHeight: '25rem'}}>
+            <Card style={{minWidth: '16rem', flexGrow: 1, margin:'1rem', minHeight: '25rem', maxHeight:'25rem'}}>
                 <Card.Img variant="top" src={imagePath} style={{padding:'1rem', maxHeight: '10rem', height:'100%', objectFit: 'contain'}} />
                 <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
                     <div>
