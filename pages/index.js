28541d3b0e7122df9d9cdded79729db2d6b1d4e8
Robby <buckdestructor@gmail.com>
2021-10-15 16:44:20 -0400

    Fixed cookies

--- pages/index.js
+++ pages/index.js
@@ -385,13 +385,6 @@ export default function Home({data}) {
 Home.getInitialProps = async ({req, res}) => {
 
     const data = parseCookies(req)
-  
-    if (res) {
-     if (Object.keys(data).length === 0 && data.constructor === Object) {
-       res.writeHead(301, { Location: "/" })
-       res.end()
-     }
-   }
    
    return {
      data: data && data,
