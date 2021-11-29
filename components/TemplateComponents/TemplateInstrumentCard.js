376d81bd966b110c4a6562bfce6fc7650c360869
Robby <buckdestructor@gmail.com>
2021-11-28 23:11:20 -0500

    fixed template mobile interface issue

--- components/TemplateComponents/TemplateInstrumentCard.js
+++ components/TemplateComponents/TemplateInstrumentCard.js
@@ -4,7 +4,7 @@ const TemplateInstrumentCard = ({name, imagePath}) => {
     
 
     return (
-            <Card style={{minWidth: '11rem', maxWidth: '11rem', flexGrow: 1, minHeight: '16rem', maxHeight: '16rem'}}>
+            <Card style={{minWidth: '11rem', flexGrow: 1, minHeight: '16rem', maxHeight: '16rem'}}>
                 <Card.Img variant="top" src={imagePath} style={{padding:'1rem', minHeight: '10rem', maxHeight: '10rem', height:'100%', objectFit: 'contain'}} />
                 <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
                     <Card.Title>{name}</Card.Title>
