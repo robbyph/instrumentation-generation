7e1f60051805627b670013f6ccdb8805cf8e0d4e
Robby <buckdestructor@gmail.com>
2022-07-13 00:21:54 -0400

    SEO Additions

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import '../styles/custom.scss'
 import '../styles/globals.scss'
 import { CookiesProvider } from "react-cookie"
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   
@@ -9,6 +10,21 @@ function MyApp({ Component, pageProps }) {
     return (
         <CookiesProvider>
             <Layout>
+            <DefaultSeo 
+            title="Instrumentation Generation" 
+            description="A tool for composers to randomly generate groups of instruments to help jumpstart creativity, as well as learn about new instruments."
+            openGraph={{
+                images: [
+                    {
+                        url: '/images/header.jpg',
+                        width: 800,
+                        height: 600,
+                        alt: "Robby's Instrumentation Generation",
+                    }
+                ],
+                site_name: "Instrumentation Generation"
+            }}
+            />
                 <Component {...pageProps} />
             </Layout>
         </CookiesProvider>
