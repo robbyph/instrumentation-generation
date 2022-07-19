3084d76cbc16777ea204ac7d6c73e74e61fee0f9
Robby <buckdestructor@gmail.com>
2022-07-19 01:00:53 -0400

    Made the header link home

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,14 @@
-import { Jumbotron } from 'react-bootstrap'
-import headerStyles from '../styles/Header.module.scss'
-
+import { Jumbotron } from "react-bootstrap";
+import headerStyles from "../styles/Header.module.scss";
 
 const Header = () => {
-    return (
-        <Jumbotron className={headerStyles.container}>
-            
-        </Jumbotron>
-    )
-}
+  return (
+    <a href="/">
+      <Jumbotron className={headerStyles.container}></Jumbotron>
+    </a>
+  );
+};
 
-export default Header
+export default Header;
 
 //<img className={headerStyles.img} src="/images/header.jpg" alt="Robby's Instrumentation Generation" draggable="false"/>
