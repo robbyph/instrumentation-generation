e6754da3ff258d1ac9263e145b7e562291eb2dd0
Robby <buckdestructor@gmail.com>
2022-07-29 16:22:46 -0400

    Fixed contact form google broke

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,33 +1,53 @@
-export default function (req, res) {
-    require('dotenv').config()
+export default async (req, res) => {
+  require("dotenv").config();
 
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      auth: {
-        user: 'instrumentationgen.feedback@gmail.com',
-        pass: process.env.password,
-      },
-      secure: true,
-    })  
-    
-    const mailData = {
-        from: 'instrumentationgen.feedback@gmail.com',
-        to: 'robbyphoover@gmail.com',
-        subject: `[Instrumentation Generation Feedback]: ${req.body.email}`,
-        text: `${req.body.subject}`,
-        html: `<div>${req.body.message}</div><p>from ${req.body.subject}</p>`
-    }  
-    
-    transporter.sendMail(mailData, function(err, info) {
-        console.log('subject: ' + req.body.subject)
-        if(err)
-        console.log(err)
-      else
-        console.log(info)
-    
-    })  
-    res.status(200)
-    res.send('success')
-  }
+  let nodemailer = require("nodemailer");
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true, //ssl
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mailData = {
+    from: {
+      name: req.body.name,
+      address: process.env.EMAIL,
+    },
+    to: "robbyphoover@gmail.com",
+    subject: `[Instrumentation Generation Feedback] ${req.body.subject}`,
+    html: `<div>${req.body.message}</div><p>From: ${req.body.name}</p>${req.body.email}`,
+  };
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+
+  res.status(200).json({ status: "OK" });
+};
--- pages/feedback.js
+++ pages/feedback.js
@@ -1,85 +1,132 @@
-import { NextSeo } from 'next-seo'
-import styles from '../styles/Feedback.module.scss'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import {useState} from 'react'
+import { NextSeo } from "next-seo";
+import styles from "../styles/Feedback.module.scss";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 
 const feedback = () => {
-    
-    const [subject, setSubject] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [submitted, setSubmitted] = useState(false)
-    const [responseText, setResponseText] = useState('')
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      setResponseText('Sending...')
-  
-      let data = {
-          subject,
-          message,
-          email
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [responseText, setResponseText] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResponseText("Sending...");
+
+    let data = {
+      name,
+      email,
+      subject,
+      message,
+    };
+
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received");
+      if (res.status === 200) {
+        setResponseText("Sent!");
+        setSubmitted(true);
+        setName("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
       }
-  
-      fetch('/api/contact', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then((res) => {
-          if (res.status === 200) {
-              setResponseText('Sent!')
-              setSubmitted(true) 
-              setSubject('')
-              setEmail('')
-              setMessage('')
-          }
-      })
-    }
+    });
+  };
 
-
-    return (
-        <div>
-            <NextSeo 
-                title="Feedback" 
-                description="A tool for composers to generate random groups of instruments to help jumpstart creativity."
+  return (
+    <div>
+      <NextSeo
+        title="Feedback"
+        description="A tool for composers to generate random groups of instruments to help jumpstart creativity."
+      />
+      <h1 className={styles.headingOne}>Feedback</h1>
+      <div className={styles.container}>
+        <p className={styles.p}>
+          Found a broken link? Have you found inaccurate info in one of the
+          instrument descriptions? Do you think that one of the instruments is
+          tagged incorrectly? Technical glitch? Need help fixing a corrupted
+          instrument list? Send me an email and I'll look into it! Or, if you'd
+          rather, open a pull request on the github page!
+          <br />
+        </p>
+        <p className={styles.p}>
+          Also! If you know an instrument that you think should be added, please
+          let me know!
+        </p>
+        <Form>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Your Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
-            <h1 className={styles.headingOne}>Feedback</h1>
-            <div className={styles.container}>
-                <p className={styles.p}>
-                    Found a broken link? Have you found inaccurate info in one of the instrument descriptions? Do you think that one of the instruments is tagged incorrectly? Technical glitch? Need help fixing a corrupted instrument list? Send me an email and I'll look into it! Or, if you'd rather, open a pull request on the github page!
-                    <br />
-                </p>
-                <p className={styles.p}>Also! If you know an instrument that you think should be added, please let me know!</p>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formEmail">
-                        <Form.Label>Your Email</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" onChange={(e)=>{setSubject(e.target.value)}}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formSubject">
-                        <Form.Label>Subject</Form.Label>
-                        <Form.Control type="textarea" onChange={(e)=>{setEmail(e.target.value)}}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formName">
-                        <Form.Label>Comments, Questions and/or Concerns</Form.Label>
-                        <Form.Control as="textarea" rows={5} onChange={(e)=>{setMessage(e.target.value)}}/>
-                    </Form.Group>
-                    <Row>
-                        <Col xl={1}>
-                            <Button  type="submit" onClick={(e)=>{handleSubmit(e)}}>Send</Button>
-                        </Col>
-                        <Col xl={1}>
-                            <hr style={{height: '0px', visibility:'hidden', margin: '.15rem'}} />
-                            <p style={{margin:0}}>{responseText}</p>
-                        </Col>
-                    </Row>
-                </Form>
-            </div>
-            
-        </div>
-    )
-}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control
+              type="textarea"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formSubject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type="textarea"
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Comments, Questions and/or Concerns</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Row>
+            <Col xl={1}>
+              <Button
+                type="submit"
+                onClick={(e) => {
+                  handleSubmit(e);
+                }}
+              >
+                Send
+              </Button>
+            </Col>
+            <Col xl={1}>
+              <hr
+                style={{
+                  height: "0px",
+                  visibility: "hidden",
+                  margin: ".15rem",
+                }}
+              />
+              <p style={{ margin: 0 }}>{responseText}</p>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default feedback
+export default feedback;
